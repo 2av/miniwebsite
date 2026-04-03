8dff0fb955d6c910b8b11796b0d46ff0a3a289e7
Akhilesh Vishwakarma <akhilesh.vis17@gmail.com>
2026-04-03 11:00:24 +0530

    asasa

--- user/website/videos.php
+++ user/website/videos.php
@@ -132,7 +132,7 @@ include '../includes/header.php';
         <div class="card mb-4">
             <div class="card-body">
             <label class="heading2">Video Links:</label>
-            <p class="description">You can add upto 20 video links related to your business</p>
+            <p class="description">You can upload upto 20 videos </p>
                 <form action="" method="POST" id="card_form">
 
                     <?php
